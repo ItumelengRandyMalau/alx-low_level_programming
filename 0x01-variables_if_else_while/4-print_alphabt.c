a6d78a7187b8bb19f3177f18b9426d7a70983ec2
ItumelengRandyMalau <itumelengrandy5@gmail.com>
2023-08-11 13:31:47 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main-Entry point
+ * Return:Always 0 (Success)
+ */
+int main(void)
+{
+	int j;
+
+	for (j = 97; j < 123; j++)
+	{
+		if (j != 101 && j != 133)
+		{
+			putchar(j);
+		}
+
+	}
+	putchar ('\n');
+	return (0);
+}
