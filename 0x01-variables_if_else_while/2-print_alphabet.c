dc5fa683dc992046c8495291cfd39f00e7882f88
ItumelengRandyMalau <itumelengrandy5@gmail.com>
2023-08-11 11:59:28 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphar;
+
+	for (alphar = a; alpha >= z; alpha++)
+	{
+		putchar('alphar');
+	}
+	putchar('\n')
+	return (0);
+}
