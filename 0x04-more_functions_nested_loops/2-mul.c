dbc81c28e18ea77de1b7049e8fbc957be2dc7a6e
ItumelengRandyMalau <itumelengrandy5@gmail.com>
2023-08-17 03:03:49 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiply two intergers
+ * @a: int1
+ * @b: int2
+ * Return: addition of the multiplicattion
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
