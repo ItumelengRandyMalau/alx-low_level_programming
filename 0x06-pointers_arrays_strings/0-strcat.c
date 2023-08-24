897bd846b071bb0cccd71ac21b1e1ecb6cafd1b7
ItumelengRandyMalau <itumelengrandy5@gmail.com>
2023-08-23 23:25:48 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,11 +11,11 @@ char *_strcat(char *dest, char *src)
 {
 	int i = 0, j = 0;
 
-	while (src[i] != '\0')
+	while (dest[i] != '\0')
 	{
 		i++;
 	}
-	while (dest[j] != '\0')
+	while (src[j] != '\0')
 	{
 		dest[j] = src[i];
 		j++;
