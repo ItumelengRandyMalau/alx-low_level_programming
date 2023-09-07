98e568ed57114c5cacfeb023e86be714b8cf4596
ItumelengRandyMalau <itumelengrandy5@gmail.com>
2023-09-07 12:51:20 -0700

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,45 +2,45 @@
 #include "main.h"
 
 /**
- * *_calloc - allocates memory for an array
- * @nmemb: number of elements in the array(input)
- * @size: size of each element(input)
- *
- * Return: pointer to allocated memory(ptr)
- */
-void *_calloc(unsigned int nmemb, unsigned int size)
-{
-	char *ptr;
-
-	if (nmemb == 0 || size == 0)
-		return (NULL);
-
-	void* ptr = malloc(size * nmemb);
-
-	if (ptr == NULL)
-		return (NULL);
-
-	_memset(ptr, 0, nmemb * size);
-
-	return (ptr);
-}
-
-/**
- * *_memset - fills memory with a constant byte
+ * _memset - fills memory with a constant byte
  * @s: memory area to be filled(input)
  * @b: char to copy(input)
  * @n: number of times to copy b(input)
  *
- * Return: pointer to the memory area s
+ * Return: pointer to the memory area (s)
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int p;
+	unsigned int j;
 
-	for (p = 0; p < n; p++)
+	for (j = 0; j < n; j++)
 	{
-		s[p] = b;
+		s[j] = b;
 	}
 
 	return (s);
+}
+
+/**
+ * _calloc - allocates memory for an array
+ * @nmemb: number of elements in the array(input)
+ * @size: size of each element(input)
+ *
+ * Return: pointer to allocated memory(arr)
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *arr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	arr = malloc(size * nmemb);
+
+	if (arr == NULL)
+		return (NULL);
+
+	_memset(arr, 0, nmemb * size);
+
+	return (arr);
 }
