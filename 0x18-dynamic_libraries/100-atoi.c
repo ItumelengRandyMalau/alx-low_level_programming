29ce023df6854d941672f75105d3e76f9570a42e
ItumelengRandyMalau <itumelengrandy5@gmail.com>
2023-12-11 20:22:41 +0000

    libdynamic.so

---
+++ 0x18-dynamic_libraries/100-atoi.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+* *_atoi: conversion of a data type
+*@s: input
+*
+*Return: 0 on success
+*/
+int _atoi(char *s)
+{
+}
---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @j: input
+ * Return: the absolute value of int
+ */
+int _abs(int j)
+{
+	if (j >= 0)
+	{
+		return (j);
+	}
+	return (-j);
+}
+
